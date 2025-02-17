1fc5e2cd082db7fcae5949d68afb90362c8d911b
dindy01 <dindiba2000@gmail.com>
2025-02-17 13:54:35 +0100

    arrayinizializzato con commenti

--- programmazione_24marzo/src/algoritmi/ordinamento1.java
+++ programmazione_24marzo/src/algoritmi/ordinamento1.java
@@ -1,25 +1,31 @@
+//Implementa Selection Sort
 package algoritmi;
 
-public class ordinamento1{
+public class ordinamento1 {
     
     public static void main(String[] args) {
         
-        int a[] = {7, 2, 5, 4, 1, 6, 0, 3};
-        int temp = 0;
+        int a[] = {7, 2, 5, 4, 6, 0, 3};
+        int temp = 0; //variable temporanea per lo swap
         
-        
+        //primo ciclo for
         for (int i = 0; i < a.length; i++) {
             
-            int min = i;
+            int min = i; //variabile per memorizzare il minimo elemento dell'array
+            
+            //secondo ciclo for per trovare il minimo
             for (int j = i + 1; j < a.length; j++) {
-                if (a[min] > a[j]) {
+                //confronto per trovare il valore minimo
+                if(a[min] > a[j]) {
+                    //se trovo il valore minimo memorizzo la sua POSIZIONE nell'array
                     min = j;
                 }
             }
-            temp = a[min];
-            a[min] = a[i];
-            a[i] = temp;
-            System.out.print(i + " ");
+            //swap
+            temp = a[min]; //memorizzo il valore minimo all'interno di una variabile temporanea
+            a[min] = a[i]; //assegno l'elemento i dell'array all'elemento dove si trovava prima il valore minimo
+            a[i] = temp; //ora nella posizione i inserisco il valore minimo
+            //alla fine dell'iterazione ilvalore di i viene incrementato e l'array comincia dal valore successivo
         }
     }
 }
