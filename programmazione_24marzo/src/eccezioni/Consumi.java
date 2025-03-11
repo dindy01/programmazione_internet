10da97d4d783a3a9d66c1237aed4f4b8ba537ce1
dindy01 <dindiba2000@gmail.com>
2025-03-11 15:15:29 +0100

    eccezioni

--- programmazione_24marzo/src/eccezioni/Consumi.java
+++ programmazione_24marzo/src/eccezioni/Consumi.java
@@ -1,26 +1,53 @@
 package eccezioni;
-
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.lang.IllegalStateException;
 public class Consumi {
 
     public static void main(String[] args) {
         java.util.Scanner tastiera = new java.util.Scanner(System.in);
-        int kilometri, litri, distanza;
+        int kilometri = 0, litri, distanza;
         
+        while(true) {
         try {
             System.out.println("Inserire i kilometri percorsi: ");
             kilometri = tastiera.nextInt();
+            break;
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Devi inserire un numero intero: " + e.getMessage());
+            tastiera.next();
+        } catch (NoSuchElementException e) {
+            System.out.println("Non è un numero." + e.getMessage());
+        } catch (IllegalStateException e) {
+            System.out.println("Scanner chiuso inaspettatamente");
+            return;
+        }
+        }
+        
+        while(true) {
+        try {
             System.out.println("Inserire i litri di benzina consumati: ");
             litri = tastiera.nextInt();
             
             distanza = kilometri / litri;
-            
             System.out.println("la tua auto fa " + distanza + " kilometri al litro");
+            break;
             
-        } catch (Exception e) {
-            System.out.println("Forse hai un problema: " + e.getMessage());
-            System.out.println("Possibile che la tua auto non consumi?");
+        }   catch (ArithmeticException e) {
+            System.out.println("Divisione per zero non possibile");
+        } catch (InputMismatchException e) {
+            System.out.println("Devi inserire un numero intero: " + e.getMessage());
+            tastiera.next();
+        } catch (NoSuchElementException e) {
+            System.out.println("Non è un numero." + e.getMessage());
+        } catch (IllegalStateException e) {
+            System.out.println("Scanner chiuso inaspettatamente");
+            return;
+        } 
         }
         
+        tastiera.close();
         System.out.println("Fine programma");
 
     }
